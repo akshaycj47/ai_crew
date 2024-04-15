153b8c744839c520c0cc99cf35e2f0199cab47a3
Akshay Sawant <asawant@gatech.edu>
2024-04-15 00:25:34 -0400

    Add another example

--- google_newsletter.py
+++ google_newsletter.py
@@ -4,7 +4,7 @@ from crewai import Agent, Task, Process, Crew
 
 from langchain.agents import Tool
 from langchain.agents import load_tools
-from langchain.utilities import GoogleSerperAPIWrapper
+from langchain_community.utilities import GoogleSerperAPIWrapper
 
 serper_api_key = os.environ.get("SERPER_API_KEY")
 
@@ -30,10 +30,10 @@ api = os.environ.get("OPENAI_API_KEY")
 """
 explorer = Agent(
     role="Senior Researcher",
-    goal="Find and explore the most exciting projects and companies in the ai and machine learning space in 2024",
+    goal="Find and explore the most exciting projects and companies in the AI and machine learning space in 2024",
     backstory="""You are and Expert strategist that knows how to spot emerging trends and companies in AI, tech and machine learning. 
     You're great at finding interesting, exciting projects on LocalLLama subreddit. You turned scraped data into detailed reports with names
-    of most exciting projects an companies in the ai/ml world. ONLY use scraped data from the internet for the report.
+    of most exciting projects an companies in the AI/ML world. ONLY use scraped data from the internet for the report.
     """,
     verbose=True,
     allow_delegation=False,
@@ -45,7 +45,7 @@ writer = Agent(
     goal="Write engaging and interesting blog post about latest AI projects using simple, layman vocabulary",
     backstory="""You are an Expert Writer on technical innovation, especially in the field of AI and machine learning. You know how to write in 
     engaging, interesting but simple, straightforward and concise. You know how to present complicated technical terms to general audience in a 
-    fun way by using layman words.ONLY use scraped data from the internet for the blog.""",
+    fun way by using layman words. ONLY use scraped data from the internet for the blog.""",
     verbose=True,
     allow_delegation=True,
 )
@@ -60,3 +60,73 @@ critic = Agent(
     verbose=True,
     allow_delegation=True,
 )
+
+task_report = Task(
+    description="""Use and summarize scraped data from the internet to make a detailed report on the latest rising projects in AI. Use ONLY 
+    scraped data to generate the report. Your final answer MUST be a full analysis report, text only, ignore any code or anything that 
+    isn't text. The report has to have bullet points and with 5-10 exciting new AI projects and tools. Write names of every tool and project. 
+    Each bullet point MUST contain 3 sentences that refer to one specific AI company, product, model or anything you found on the internet.  
+    """,
+    agent=explorer,
+    expected_output="Your final answer MUST be a full analysis report, text only, ignore any code or anything that isn't text. The report has to have bullet points and with 5-10 exciting new AI projects and tools."
+)
+
+task_blog = Task(
+    description="""Write a blog article with text only and with a short but impactful headline and at least 10 paragraphs. Blog should summarize 
+    the report on latest ai tools found on localLLama subreddit. Style and tone should be compelling and concise, fun, technical but also use 
+    layman words for the general public. Name specific new, exciting projects, apps and companies in AI world. Don't 
+    write "**Paragraph [number of the paragraph]:**", instead start the new paragraph in a new line. Write names of projects and tools in BOLD.
+    ALWAYS include links to projects/tools/research papers. ONLY include information from LocalLLAma.
+    """,
+    agent=writer,
+    expected_output="""
+    For your Outputs use the following markdown format:
+    ```
+    ## [Title of post](link to project)
+    - Interesting facts
+    - Own thoughts on how it connects to the overall theme of the newsletter
+    ## [Title of second post](link to project)
+    - Interesting facts
+    - Own thoughts on how it connects to the overall theme of the newsletter
+    ```
+    """
+)
+
+task_critique = Task(
+    description="""The Output MUST have the following markdown format:
+    ```
+    ## [Title of post](link to project)
+    - Interesting facts
+    - Own thoughts on how it connects to the overall theme of the newsletter
+    ## [Title of second post](link to project)
+    - Interesting facts
+    - Own thoughts on how it connects to the overall theme of the newsletter
+    ```
+    Make sure that it does and if it doesn't, rewrite it accordingly.
+    """,
+    agent=critic,
+    expected_output="""The Output MUST have the following markdown format:
+    ```
+    ## [Title of post](link to project)
+    - Interesting facts
+    - Own thoughts on how it connects to the overall theme of the newsletter
+    ## [Title of second post](link to project)
+    - Interesting facts
+    - Own thoughts on how it connects to the overall theme of the newsletter
+    ```
+    """
+)
+
+# Instantiate crew of agents
+crew = Crew(
+    agents=[explorer, writer, critic],
+    tasks=[task_report, task_blog, task_critique],
+    verbose=2,
+    process=Process.sequential,  # Sequential process will have tasks executed one after the other and the outcome of the previous one is passed as extra content into this next.
+)
+
+# Get your crew to work!
+result = crew.kickoff()
+
+print("######################")
+print(result)
